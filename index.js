819b68ec4e6e13828e16625b09e550cb8c53c9a7
tolgacesur <ttolgacesur@gmail.com>
2017-11-27 02:51:25 +0300

    edited

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
+var port = process.env.PORT || 8080;
+console.log("go");
 app.use(express.static(path.join(__dirname, 'dist')));
-
-app.listen(process.env.PORT || 3000, () => {
-};
+console.log("none");
+app.listen(port, function() {
+    console.log('Uygulama Başladı' + port);
+});
