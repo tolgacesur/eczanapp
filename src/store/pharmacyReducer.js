3df61abe658ee38252fb287b05cd5101e335e8a5
tolgacesur <ttolgacesur@gmail.com>
2017-11-27 23:10:31 +0300

    pharmacy reducer created

---
+++ src/store/pharmacyReducer.js
@@ -0,0 +1,20 @@
+const PHARMACY_INIT = 'PHARMACY_INIT';
+
+/// action creater
+export function pharmacyInit (pharmacy = {}) {
+    return {
+        type: PHARMACY_INIT,
+        payload: pharmacy
+    }
+}
+
+/// Reducer
+export default function pharmacyReducuer (state = {},action){
+    switch (action.type) {
+        case PHARMACY_INIT:
+            return Object.assign({},action.payload);
+    
+        default:
+            return state;
+    }
+}
