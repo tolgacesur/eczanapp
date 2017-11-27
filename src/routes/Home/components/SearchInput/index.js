2d0390244a7d3602a979c926c63faa57d01c7c00
tolgacesur <ttolgacesur@gmail.com>
2017-11-27 23:11:56 +0300

    pharmacy reducer connected with this component and edited component

--- src/routes/Home/components/SearchInput/index.js
+++ src/routes/Home/components/SearchInput/index.js
@@ -1,38 +1,60 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { pharmacyInit } from 'store/pharmacyReducer'
 
 class SearchInput extends Component {
 
     constructor() {
         super();
 
+        this.state = {
+            city: ""
+        }
+
     }
 
-    Getfunction = () => {
-        console.log("search event")
+    cityChanged(e) {
+        this.setState({
+            city : e.target.value
+        })
     }
 
+
+    getPharmacy(){
+        // http Call
+        console.log(this.state)
+        const pharmacy = {
+            name: "eczane adı",
+            adres: "Aşıkpaşa Mah.",
+            telefon: "+905072293584"
+        }
+
+        this.props.pharmacyData(pharmacy);
+    }
+    
 
     render() {
 
-        return <div>
-            <form className="form-inline">
-                <div className="form-group mx-sm-3">
-                    <input type="text" className="form-control" placeholder="Şehir adı yazın" />
-                </div>
-                <button type="submit" className="btn btn-primary search-btn" onClick={e => {
-                    e.preventDefault();
-                    this.Getfunction();
-                }}>Ara</button>
-                <a href="#" onClick={e => {
-                    e.preventDefault();
-                    onViewChange(3);
-                }}>Şifremi Unuttum!</a>
-            </form>
+        return (
+            <div>
+                <form className="form-inline">
+                    <div className="form-group mx-sm-3">
+                        <input type="text" className="form-control" onChange={this.cityChanged.bind(this)} value={this.state.city} placeholder="Şehir adı yazın" />
+                    </div>
+                    <button type="button" className="btn btn-primary search-btn" onClick={this.getPharmacy.bind(this)} >Ara</button>
+                </form>
 
 
-        </div>
+            </div>
+        )
 
     }
 }
 
-export default SearchInput;
+const mapDispatchToProps = (dispatch) => {
+    return {
+        pharmacyData: (pharmacy) => dispatch(pharmacyInit(pharmacy))
+    }
+}
+
+export default connect(undefined, mapDispatchToProps)(SearchInput);
