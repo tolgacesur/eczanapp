4856f83ab77fa004fb72b114bfd83838d8f1a787
tolgacesur <ttolgacesur@gmail.com>
2017-11-28 22:17:40 +0300

    index file added

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ var path = require('path');
 var app = express();
 
 var port = process.env.PORT || 8080;
-console.log("go");
+
 app.use(express.static(path.join(__dirname, 'dist')));
-console.log("none");
+
 app.listen(port, function() {
     console.log('Uygulama Başladı' + port);
 });
