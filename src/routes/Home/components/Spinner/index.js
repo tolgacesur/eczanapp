1c398917e736a14fedaab4753f6f008ed54365d4
tolgacesur <ttolgacesur@gmail.com>
2017-11-28 17:08:52 +0300

    spinner component created

---
+++ src/routes/Home/components/Spinner/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+class Spinner extends React.Component {
+
+    render () {
+        return (
+        <div>Yükleniyor</div>
+        )
+    }
+}
+
+export default Spinner;
